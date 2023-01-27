3f9bd3790aa95b1e592535355aa42419e7dd0b92
Bronwen27 <118174027+Bronwen27@users.noreply.github.com>
2023-01-27 22:05:34 +0000

    hour sections created and save button storing informtation to localstorage.

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,27 @@
 // Today's date added to head of page
 $('#currentDay').text(moment().format('DDD MMM YYYY')) 
 
+$(document).ready(function(){
 
+    //save button
+    $(".saveBtn").on("click", function(){
+
+        let timeID = $(this).parent().attr("id")
+        let value = $(this).siblings(".description").val();
+
+        console.log(timeID);
+        console.log(value);
+
+        localStorage.setItem(timeID, value);
+
+        $(".notification")
+
+        setTimeout(function(){
+            $(".notification").removeClass("show");
+        }, 5000)
+    })
+
+    function hourUpdate() {
+        
+    }
+})
