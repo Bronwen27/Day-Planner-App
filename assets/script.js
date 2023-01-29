2b168da7041ae35e6c0af7efffe5e0aff2d36180
Bronwen27 <118174027+Bronwen27@users.noreply.github.com>
2023-01-29 02:18:11 +0000

    tried another time code method but hasnt worked

--- assets/script.js
+++ assets/script.js
@@ -23,59 +23,22 @@ $(document).ready(function(){
 
     // set color coded rows according to updating time
 
-   const rows = document.getElementsByClassName("row");
-let currentHour = parseInt(moment().format('H'));
 
-Array.from(rows).forEach(row => {
-  let
-    rowIdString = row.id,
-    rowHour;
-  if (rowIdString) {
-    rowHour = parseInt(rowIdString);
-  }
-  if (rowHour) {
-    // Compares row id to current hour and sets color accordingly
-    if (currentHour === rowHour) {
-      setColor(row, "red");
-    } else if ((currentHour < rowHour) && (currentHour > rowHour - 6)) {
-      setColor(row, "green");
-    } else if ((currentHour > rowHour) && (currentHour < rowHour + 6)) {
-      setColor(row, "lightgrey");
-    } else {
-      setColor(row, "white");
-    }
-  }
-});
+    function hourUpdate() {
+        var hour = moment().hours();
+    
+        $(".time-block").each(function() {
+            var currHour = parseInt($(this).attr("id"));
 
-function setColor(element, color) {
-  element.style.backgroundColor = color;
-}
-
-    /*function hourTracker(){
-        let currentHour = moment().hours();
-              $(".time-block").each(function () {
-            var rowHour = parseInt($(".time-block").attr("id").split("hour")[1]);
-            console.log( rowHour, currentHour)
-
-            //check if we've moved past this time, click into css/html given classes of past, present, or future
-            if (rowHour < currentHour) {
-                $(this).addClass("past");
-                $(this).removeClass("future");
-                $(this).removeClass("present");
-            }
-            else if (rowHour === currentHour) {
-                $(this).removeClass("past");
-                $(this).addClass("present");
-                $(this).removeClass("future");
-            }
-            else {
-                $(this).removeClass("present");
-                $(this).removeClass("past");
+    
+            if (currHour > hour) {
                 $(this).addClass("future");
+            } else if (currHour === hour) {
+                $(this).addClass("present");
+            } else {
+                $(this).addClass("past");
             }
         })
-    }
-    hourTracker(); 
-    */
-}
-)
+    };
+
+})
