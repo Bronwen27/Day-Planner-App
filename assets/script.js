261dcd5674cce6581b3c036e752763d780829c0b
Bronwen27 <118174027+Bronwen27@users.noreply.github.com>
2023-01-29 01:47:40 +0000

    started code for color coding time- not working as planned, need to add classes

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,9 @@
 // Today's date added to head of page
-$('#currentDay').text(moment().format('DDD MMM YYYY')) 
+$('#currentDay').text(moment().format('MMMM Do YYYY, h:mm:ss a'));
 
 $(document).ready(function(){
 
-    //save button
+    // save button function
     $(".saveBtn").on("click", function(){
 
         let timeID = $(this).parent().attr("id")
@@ -19,9 +19,63 @@ $(document).ready(function(){
         setTimeout(function(){
             $(".notification").removeClass("show");
         }, 5000)
-    })
+    });
 
-    function hourUpdate() {
-        
+    // set color coded rows according to updating time
+
+   const rows = document.getElementsByClassName("row");
+let currentHour = parseInt(moment().format('H'));
+
+Array.from(rows).forEach(row => {
+  let
+    rowIdString = row.id,
+    rowHour;
+  if (rowIdString) {
+    rowHour = parseInt(rowIdString);
+  }
+  if (rowHour) {
+    // Compares row id to current hour and sets color accordingly
+    if (currentHour === rowHour) {
+      setColor(row, "red");
+    } else if ((currentHour < rowHour) && (currentHour > rowHour - 6)) {
+      setColor(row, "green");
+    } else if ((currentHour > rowHour) && (currentHour < rowHour + 6)) {
+      setColor(row, "lightgrey");
+    } else {
+      setColor(row, "white");
     }
-})
+  }
+});
+
+function setColor(element, color) {
+  element.style.backgroundColor = color;
+}
+
+    /*function hourTracker(){
+        let currentHour = moment().hours();
+              $(".time-block").each(function () {
+            var rowHour = parseInt($(".time-block").attr("id").split("hour")[1]);
+            console.log( rowHour, currentHour)
+
+            //check if we've moved past this time, click into css/html given classes of past, present, or future
+            if (rowHour < currentHour) {
+                $(this).addClass("past");
+                $(this).removeClass("future");
+                $(this).removeClass("present");
+            }
+            else if (rowHour === currentHour) {
+                $(this).removeClass("past");
+                $(this).addClass("present");
+                $(this).removeClass("future");
+            }
+            else {
+                $(this).removeClass("present");
+                $(this).removeClass("past");
+                $(this).addClass("future");
+            }
+        })
+    }
+    hourTracker(); 
+    */
+}
+)
